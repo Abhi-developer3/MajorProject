c79634c15616ec6f1e596c11e03ffbf08d886f84
Abhi-developer3 <menkaabhishek03@gmail.com>
2025-11-29 13:00:24 +0530

    Update server.js
    changes in cores

--- resume-builder/server/server.js
+++ resume-builder/server/server.js
@@ -13,7 +13,13 @@ const PORT = process.env.PORT || 3000;
 await connectDB()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    
+  origin: "https://frontend-1jzp.onrender.com/",
+  credentials: true
+
+
+}));
 
 app.get('/', (req, res)=> res.send("Server is live..."))
 app.use('/api/users', userRouter)
